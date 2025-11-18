b89847498a084258f58d2d44e4eb7b06817d688b
Anjlodd <jokerrdd@outlook.com>
2025-11-18 02:36:24 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 ./SRBMiner-MULTI \
 --algorithm randomx \
 --pool rx.unmineable.com:3333 \
---wallet ETC:0xe02cea6dc46e6a12f9baa11094f10ced5b6c3eab.LINUX#x \
+--wallet ETC:0xe02cea6dc46e6a12f9baa11094f10ced5b6c3eab.GX_VM3#x \
 --cpu-threads 4 \
 --cpu-priority 5 \
 --log-file logs.txt
